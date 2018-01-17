763c60533600361683d031cdf31f58919593a3df
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-01-16 23:07:06 -0700

    Style basic form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Form.css';
 
   class Form extends Component {
   constructor(props) {
@@ -17,43 +18,45 @@ import React, { Component } from 'react';
   render() {
     return (
       <div className="form">
-        <form>
-          <div className="form__names">
-            <label>
-              <span>First name</span>
-              <input type="text" name="firstname" />
-            </label>
-            <label>
-              <span>Last name</span>
-              <input type="text" name="lastname" />
-            </label>
-          </div>
-          <div className="form__extra">
-            <div className="form__attendance">
-              <span>Will you be attending?</span>
+        <div className="form__inner">
+          <form>
+            <div className="form__names">
               <label>
-                <input type="radio"
-                name="attendance" value="yes"
-                checked={this.state.attendingOption === 'yes'}
-                onChange={this.handleOptionChange}/>
-                <span>Yes</span>
+                <span>First name</span>
+                <input type="text" name="firstname" />
               </label>
               <label>
-                <input type="radio"
-                name="attendance" value="no"
-                checked={this.state.attendingOption === 'no'}
-                onChange={this.handleOptionChange}/>
-                <span>No</span>
+                <span>Last name</span>
+                <input type="text" name="lastname" />
               </label>
             </div>
-            {this.state.attendingOption === 'yes' &&
-              <label>
-                <span>How many guests will be in your party?</span>
-                <input type="number" name="guests" min="1" max="10" />
-              </label>
-            }
-          </div>
-        </form>
+            <div className="form__extra">
+              <div className="form__attendance">
+                <span>Will you be attending?</span>
+                <label>
+                  <input type="radio"
+                  name="attendance" value="yes"
+                  checked={this.state.attendingOption === 'yes'}
+                  onChange={this.handleOptionChange}/>
+                  <span>Yes</span>
+                </label>
+                <label>
+                  <input type="radio"
+                  name="attendance" value="no"
+                  checked={this.state.attendingOption === 'no'}
+                  onChange={this.handleOptionChange}/>
+                  <span>No</span>
+                </label>
+              </div>
+              {this.state.attendingOption === 'yes' &&
+                <label>
+                  <span>How many guests will be in your party?</span>
+                  <input type="number" name="guests" min="1" max="10" />
+                </label>
+              }
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
