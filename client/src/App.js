1d2964e8470c95d4ff185818300c86917b479fe6
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-01-21 20:20:11 -0700

    Add express backend server and start running

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,32 @@ import Form from './Form/Form';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
+    
+    
     return (
       <div className="Wedding">
+      <p className="App-intro">{this.state.response}</p>
         <header className="Wedding-header">
           <Header />
         </header>
--- client/src/Form/Form.js
+++ client/src/Form/Form.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './Form.css';
 
-  class Form extends Component {
+class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      attendingOption: 'yes',
       activeFirst: false,
-      activeLast: false
+      activeLast: false,
+      firstName: '',
+      lastName: '',
+      attendingOption: 'yes',
+      numberAttending: '',
+      memo: '',
+      formSent: false
     };
   }
 
@@ -23,7 +28,7 @@ import './Form.css';
   }
   handleInputFilled = (e) => {
     if(e.target.value !== '') {
-      return (e.target.name === 'firstname') ? this.setState({activeFirst: true}) : this.setState({activeLast: true})
+      return (e.target.name === 'firstname') ? this.setState({activeFirst: true, firstName: e.target.value}) : this.setState({activeLast: true, lastName: e.target.value})
     }
     return (e.target.name === 'firstname') ? this.setState({activeFirst: false}) : this.setState({activeLast: false})
   }
@@ -34,13 +39,54 @@ import './Form.css';
     });
   }
 
+  handleNumberAttendingChange = (e) => {
+    this.setState({
+      numberAttending: e.target.value
+    });
+  }
+
+  handleMemoChange = (e) => {
+    this.setState({
+      memo: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    
+    fetch('/thank-you', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        attendingOption: this.state.attendingOption,
+        numberAttending: this.state.numberAttending,
+        memo: this.state.memo,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.success) {
+        this.setState({formSent: true})
+      }
+      else this.setState({formSent: false})
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     return (
       <div className="form">
         <div className="form__inner">
           <h2>We'd love for you to join us!</h2>
           <h3>Please RSVP here</h3>
-          <form id="rsvp">
+          <form method="POST" id="rsvp" onSubmit={this.handleSubmit}>
             <div className="form__names">
               <label className={this.state.activeFirst ? 'active' : ''}>
                 <span>First name *</span>
@@ -56,7 +102,7 @@ import './Form.css';
                 <span>Last name *</span>
                 <input
                   type="text"
-                  name="lastname"
+                  name="N"
                   required
                   onBlur={this.handleBlurEvent}
                   onFocus={this.handleFocusEvent}
@@ -74,7 +120,7 @@ import './Form.css';
               <label className={this.state.attendingOption === 'no' ? 'active' : ''}>
                 <input type="radio"
                 name="attendance" value="no"
-                onChange={this.handleOptionChange}/>
+                onChange={this.handleOptionChange} />
                 <span>No</span>
               </label>
             </div>
@@ -82,15 +128,28 @@ import './Form.css';
               <div className="form__number">
                 <label>
                   <span>How many guests will be in your party? *</span>
-                  <input required type="number" name="guests" min="1" max="10" />
+                  <input required 
+                    type="number" 
+                    name="guests" 
+                    min="1" max="10" 
+                    onChange={this.handleNumberAttendingChange} />
                 </label>
               </div>
             }
             <div className="form__memo">
               <textarea name="memo" placeholder='Send us a note! (optional)'></textarea>
             </div>
-            <button type="submit" form="rsvp" value="Submit">Submit</button>
+            <button
+              type="submit" 
+              form="rsvp" 
+              value="Submit"
+              onChange={this.handleMemoChange} >
+                Submit
+            </button>
           </form>
+          {this.state.formSent &&
+            <div>Thank you, form sent!</div>
+          }
         </div>
       </div>
     );
