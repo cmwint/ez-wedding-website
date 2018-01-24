5020a366e2beca3ac0ce95c465e3a1734d31eb9d
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-01-23 22:30:52 -0700

    Add mailgun and some vars in server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+var Mailgun = require('mailgun-js');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//Your api key, from Mailgun’s Control Panel
+var api_key = 'MAILGUN-API-KEY';
+
+//Your domain, from the Mailgun Control Panel
+var domain = 'lovingyouisez.com';
+
+//Your sending email address
+var from_who = 'guest@lovingyouisez.com';
+
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
