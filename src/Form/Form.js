78ef8a6a771e8989572a0a924b37a061703aa9f1
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-01-28 13:00:39 -0700

    Add link to google form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,99 +1,19 @@
 import React, { Component } from 'react';
 import './Form.css';
 
-  class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      attendingOption: 'yes',
-      activeFirst: false,
-      activeLast: false
-    };
-  }
-
-  handleBlurEvent = (e) => {
-    if(e.target.value !== '') {
-      return (e.target.name === 'firstname') ? this.setState({activeFirst: true}) : this.setState({activeLast: true})
-    }
-    return (e.target.name === 'firstname') ? this.setState({activeFirst: false}) : this.setState({activeLast: false})
-  }
-  
-  handleFocusEvent = (e) => {
-    return (e.target.name === 'firstname') ? this.setState({activeFirst: true}) : this.setState({activeLast: true})
-  }
-  handleInputFilled = (e) => {
-    if(e.target.value !== '') {
-      return (e.target.name === 'firstname') ? this.setState({activeFirst: true}) : this.setState({activeLast: true})
-    }
-    return (e.target.name === 'firstname') ? this.setState({activeFirst: false}) : this.setState({activeLast: false})
-  }
-
-  handleOptionChange = (e) => {
-    this.setState({
-      attendingOption: e.target.value
-    });
-  }
+class Form extends Component {
 
   render() {
     return (
       <div className="form">
         <div className="form__inner">
           <h2>We'd love for you to join us!</h2>
-          <h3>Please RSVP here</h3>
-          <form id="rsvp">
-            <div className="form__names">
-              <label className={this.state.activeFirst ? 'active' : ''}>
-                <span>First name *</span>
-                <input
-                  type="text"
-                  name="firstname"
-                  required
-                  onBlur={this.handleBlurEvent}
-                  onFocus={this.handleFocusEvent}
-                  onChange={this.handleInputFilled} />
-              </label>
-              <label className={this.state.activeLast ? 'active' : ''}>
-                <span>Last name *</span>
-                <input
-                  type="text"
-                  name="lastname"
-                  required
-                  onBlur={this.handleBlurEvent}
-                  onFocus={this.handleFocusEvent}
-                  onChange={this.handleInputFilled} />
-              </label>
-            </div>
-            <div className="form__attendance">
-              <span>Will you be attending?</span>
-              <label className={this.state.attendingOption === 'yes' ? 'active' : ''}>
-                <input type="radio"
-                name="attendance" value="yes"
-                onChange={this.handleOptionChange}/>
-                <span>Yes</span>
-              </label>
-              <label className={this.state.attendingOption === 'no' ? 'active' : ''}>
-                <input type="radio"
-                name="attendance" value="no"
-                onChange={this.handleOptionChange}/>
-                <span>No</span>
-              </label>
-            </div>
-            {this.state.attendingOption === 'yes' &&
-              <div className="form__number">
-                <label>
-                  <span>How many guests will be in your party? *</span>
-                  <input required type="number" name="guests" min="1" max="10" />
-                </label>
-              </div>
-            }
-            <div className="form__memo">
-              <textarea name="memo" placeholder='Send us a note! (optional)'></textarea>
-            </div>
-            <button type="submit" form="rsvp" value="Submit">Submit</button>
-          </form>
+          <h3>Please visit <a href="https://docs.google.com/forms/d/e/1FAIpQLSd2LkZmC78s-OR0sH2KxjlflU2ys-Dak0C3pwS-0d2MifdmZA/viewform" target="_blank">this form</a> to RSVP for the wedding.
+          </h3>
         </div>
       </div>
     );
   }
 }
+
 export default Form;
