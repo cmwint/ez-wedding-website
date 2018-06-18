e93e8bbda2123ecbcf849a9b8d313ada2a933a0d
Corinne Winthrop <corinne.winthrop@gmail.com>
2018-06-18 17:42:34 -0600

    Change wedding time

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -14,7 +14,7 @@ class Details extends Component {
               <img src={chapelSVG} alt='Wedding chapel' />
             </div>
             <h2>The Ceremony</h2>
-            <h4>5:00pm</h4>
+            <h4>5:30pm</h4>
             <p>
               At Ralston’s Crossing Chapel
               <a href="https://www.google.com/maps/place/Ralston%E2%80%99s+Crossing+Event+Center/@39.8311025,-105.1666335,17z/data=!3m1!4b1!4m5!3m4!1s0x876b8f038f837b1f:0x3bba13873185cfec!8m2!3d39.8311025!4d-105.1644448" target="_blank" rel="noopener noreferrer">
